13f2aabad88b414ae973e2e26c359bbda04ada68
toombez <timurtokaev.toombez@gmail.com>
2022-04-02 04:39:24 +0300

    Add OptionParameter component

---
+++ src/components/OptionParameter.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <label :for="parameter?.ID">{{ parameter?.description }} ({{ parameter?.name }})</label>
+    <input
+        :type="parameter?.Type"
+        :min="parameter?.min"
+        :max="parameter?.max"
+        :step="parameter?.step"
+        :id="parameter?.ID"
+        v-model="parameter!.value"
+    >
+</div>
+</template>
+<script lang="ts" setup>
+import { defineProps, PropType } from 'vue';
+import OptionParameter from '@/structures/OptionParameter';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps({
+    parameter: Object as PropType<OptionParameter<number | string>>
+});
+</script>
