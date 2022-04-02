b7f0bd8de8a7e8bcc67d924275a6b98dc743eb9a
toombez <timurtokaev.toombez@gmail.com>
2022-04-02 04:37:05 +0300

    Refactor OptionParameter class
    Each inherited class must implement a method to increase
    its counter, as well as generate IDs for instances.

--- src/assets/types.ts
+++ src/assets/types.ts
@@ -12,3 +12,12 @@ export type OptionGraphData = {
     call: unknown[],
     put: unknown[],
 }
+
+export interface IOptionParameterOptions {
+    name: string;
+    description?: string;
+    value: number | string;
+    min?: number | string;
+    max?: number | string;
+    step?: number;
+}
--- src/structures/OptionParameter.ts
+++ src/structures/OptionParameter.ts
@@ -1,11 +1,36 @@
-export default abstract class OptionParameter {
-    constructor(
-        public readonly type: string,
-        public readonly name: string,
-        public readonly value: number | string,
-        public readonly description?: string,
-        public readonly min?: number | string,
-        public readonly max?: number | string,
-        public readonly step?: number,
-    ) {}
+import { IOptionParameterOptions } from "@/assets/types";
+
+export default abstract class OptionParameter<T extends string | number> {
+    protected static count = 1;
+    protected abstract readonly type: string;
+
+    public readonly name!: string;
+    public readonly description?: string;
+    private id!: string;
+
+    public value!: T;
+    public readonly min?: T;
+    public readonly max?: T;
+    public readonly step?: number;
+
+    constructor(options: IOptionParameterOptions) {
+        Object.assign(this, options);
+
+        this.id = this.generateID();
+        this.increaseCount();
+    }
+
+    public get ID(): string {
+        return this.id;
+    }
+    public get Type(): string {
+        return this.type;
+    }
+
+    public abstract get Value(): number | string;
+    public abstract set Value(value: number | string);
+    protected abstract generateID(): string;
+    protected abstract increaseCount(): void;
+
+
 }
