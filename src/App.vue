5e35c36598e74bbecd4c8d1bff6f00410326b531
toombez <timurtokaev.toombez@gmail.com>
2022-04-05 23:40:58 +0300

    Add first part of application
    App using composition API with setup script.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
 <div>
-
+    <ComparingOptionModelsGraph
+        :models="[
+            BlackScholesOptionModel,
+            CoxRossRubinsteinOptionModel,
+        ]"
+    />
 </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-    name: 'App',
-});
+<script lang="ts" setup>
+import ComparingOptionModelsGraph from '@/components/ComparingOptionModelsGraph.vue';
+import BlackScholesOptionModel from '@/structures/OptionModels/BlackScholesOptionModel';
+import CoxRossRubinsteinOptionModel from '@/structures/OptionModels/CoxRossRubinsteinOptionModel';
 </script>
