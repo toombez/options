30a30ec8ab9bbb4dbf00d10cf5e8645360b168fe
toombez <timurtokaev.toombez@gmail.com>
2022-04-29 01:25:45 +0300

    Create inputs based on parameters props
    Computed parameters contains parameters objects filtred by passed props.

--- src/components/OptionForm.vue
+++ src/components/OptionForm.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { FormKitFrameworkContext } from '@formkit/core'
 import { defineProps, PropType } from 'vue'
+import { computed } from '@vue/reactivity';
 
-import { OptionParameters } from '@/assets/types';
+import { IOptionParameter, OptionParameters } from '@/assets/types';
+import ParametersSettings from '@/assets/JSON/ParametersSettings.json'
 
 interface optionFormContext extends FormKitFrameworkContext {
     parameters: OptionParameters[]
@@ -13,9 +15,25 @@ const props = defineProps({
     }
 })
 
+const parameters = computed(() => (
+    (ParametersSettings as IOptionParameter[]).filter(parameter => (
+        props.context?.parameters.includes(parameter.name as OptionParameters)
+    ))
+))
 </script>
 
 <template>
-<form>
-</form>
+<FormKit
+    v-for="parameter in parameters"
+    :key="parameter.name"
+
+    :type="parameter.type"
+    :label="parameter.label"
+    :name="parameter.name"
+    :help="parameter.description"
+
+    :step="parameter.step"
+    :min="parameter.min"
+    :max="parameter.max"
+/>
 </template>
