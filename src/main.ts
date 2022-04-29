d780565a64ad0eeab4d391e801435acfc4019da0
toombez <timurtokaev.toombez@gmail.com>
2022-04-29 03:56:29 +0300

    Register limits input type

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { plugin, defaultConfig } from '@formkit/vue'
+import { plugin, defaultConfig, createInput } from '@formkit/vue'
+
+import LimitsForm from '@/components/LimitsForm.vue'
 
 createApp(App)
-    .use(plugin, defaultConfig)
+    .use(plugin, defaultConfig({
+        inputs: {
+            limits: createInput(LimitsForm)
+        }
+    }))
     .mount('#app')
