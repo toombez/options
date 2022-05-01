7a429c195f145ecf91cf99138991c42849382edd
toombez <timurtokaev.toombez@gmail.com>
2022-05-02 00:31:28 +0300

    Add static class OptionCalculator
    Calculator calculate data for graph based on passed models,
    price types and graph length.

---
+++ src/structures/OptionCalculator.ts
@@ -0,0 +1,43 @@
+import { IOptionModel, IComparingOptionGraphData, OptionPrice } from "@/assets/types";
+
+export default abstract class OptionCalculator {
+    public static graphDataForComparing(
+        models: IOptionModel[],
+        priceTypes: OptionPrice[],
+        graphLength: number
+    ): IComparingOptionGraphData[] {
+        const result: IComparingOptionGraphData[] = []
+
+        for (const model of models.values()) {
+            for (const priceType of priceTypes.values()) {
+                const modelName = model.name
+                const data: number[] = this.callPrices(model, priceType, graphLength)
+
+                result.push({
+                    modelName,
+                    data,
+                    priceType
+                })
+            }
+        }
+
+        return result
+    }
+    private static callPrices(
+        model: IOptionModel,
+        priceType: OptionPrice,
+        graphLength: number
+    ) {
+        const data: number[] = []
+
+        const dataFunction = priceType === OptionPrice.call ?
+            model.callPrice :
+            model.putPrice
+
+        for (let index = 0; index < graphLength; index++) {
+            data.push(dataFunction.bind(model)(graphLength))
+        }
+
+        return data
+    }
+}
