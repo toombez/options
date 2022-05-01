f2362acbe9b2ed0259c54cdf285afc16b919368c
toombez <timurtokaev.toombez@gmail.com>
2022-05-02 01:27:17 +0300

    Refactor app
    Using vue-chartjs intead vue-chart-3

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
 <div>
-    <ComparingOptionModelsGraph
-        :models="[
-            BlackScholesOptionModel,
-            CoxRossRubinsteinOptionModel,
+    <FormKit
+        type="option" 
+        v-model="option"
+        :parameters="[
+            OptionParameters.r,
+            OptionParameters.sigma,
+            OptionParameters.T,
         ]"
+    />
+    <ComparingOptionModelsGraph
+        v-if="option"
+        :models="models"
     />
 </div>
 </template>
-
 <script lang="ts" setup>
-import ComparingOptionModelsGraph from '@/components/ComparingOptionModelsGraph.vue';
+import { ref } from '@vue/reactivity'
+import { watch } from 'vue';
+
+import ComparingOptionModelsGraph from '@/components/ComparingOptionModelsGraph.vue'
+
 import BlackScholesOptionModel from '@/structures/OptionModels/BlackScholesOptionModel';
 import CoxRossRubinsteinOptionModel from '@/structures/OptionModels/CoxRossRubinsteinOptionModel';
+
+import { IOptionForModel, OptionParameters } from '@/assets/types'
+
+const option = ref()
+
+watch(option, () => {
+    for (let model of models.value.values()) {
+        model.Option = option.value
+    }
+})
+
+const models = ref([
+    new BlackScholesOptionModel(option.value as IOptionForModel),
+    new CoxRossRubinsteinOptionModel(option.value as IOptionForModel)
+])
 </script>
