6aa537bddaf3ac78435da32c1fb7305269bdf243
toombez <timurtokaev.toombez@gmail.com>
2022-05-02 01:23:19 +0300

    Refactor ComparingOptionModelsGraph component
    Use vue-chartjs instead vue-chart-3 for drawing chart.
    Use calculator for get datasets. Component now waiting models as prop.
    All other data as like limits, price type contains inside in
    formkit inputs.

--- src/components/ComparingOptionModelsGraph.vue
+++ src/components/ComparingOptionModelsGraph.vue
@@ -1,80 +1,68 @@
-<template>
-<OptionCreatorForm
-    v-model="option"
-/>
+<script setup lang="ts">
+import { defineProps, PropType } from 'vue'
+import { computed, ref } from '@vue/reactivity'
 
-<LineChart
-    v-if="option && limits"
-    :chartData="graphData" 
-/>
+import { Line } from 'vue-chartjs'
+import { Chart, ChartData, registerables } from 'chart.js'
 
-<FormKit type="limits" v-model="limits" />
-</template>
+import OptionCalculator from '@/structures/OptionCalculator'
+import { ILimits, IOptionModel, OptionPrice } from '@/assets/types';
 
-<script lang="ts" setup>
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import BlackScholesOptionModel from '@/structures/OptionModels/BlackScholesOptionModel';
-import CoxRossRubinsteinOptionModel from '@/structures/OptionModels/CoxRossRubinsteinOptionModel';
-// Vue imports section
-import { defineProps, PropType, watch } from 'vue';
-
-// Charts imports section
-import { LineChart } from 'vue-chart-3';
-import { Chart, ChartData, registerables } from 'chart.js';
-
-import GraphLimits from '@/components/GraphLimits.vue';
-import OptionCreatorForm from '@/components/OptionCreatorForm.vue';
-import { computed, ref } from '@vue/reactivity';
-import { ILimits } from '@/assets/types';
-import Option from '@/structures/Option';
-import OptionModel from '@/structures/OptionModels/OptionModel';
-
-Chart.register(...registerables);
+Chart.register(...registerables)
 
 const props = defineProps({
-    models: Object as PropType<
-        (typeof CoxRossRubinsteinOptionModel | 
-        typeof BlackScholesOptionModel)[]
-    >
-});
-
-let limits = ref<ILimits>();
-let option = ref<Option>();
-
-let models: (OptionModel)[] = [];
-
-watch(option, (newOption) => {
-    models = [];
-    props.models?.map(Model => models.push(new Model(newOption!)));
-});
-
-let graphData = computed((): ChartData => {
-    const labels: number[] = [];
-    
-    for (let i = limits.value!.start; i < limits.value!.end + 1; i++) {
-        labels.push(i);
-    }
-    const datasets = models.map(model => {
-        const modelData = model.generateGraphData(limits.value!.end + 1);
-
-        return [{
-            label: modelData.label + ' call',
-            data: modelData.call.splice(limits.value!.start, limits.value!.end + 1),
-            borderColor: modelData.lineColor,
-            pointStyle: modelData.pointStyle,
-        },
-        {
-            label: modelData.label + ' put',
-            data: modelData.put.splice(limits.value!.start, limits.value!.end + 1),
-            borderColor: modelData.lineColor,
-            pointStyle: modelData.pointStyle,
-        }]
-    })
-        .flat();
-
-    return {
-        labels,
-        datasets,
+    models: {
+        type: Array as PropType<IOptionModel[]>,
+        required: true
     }
 })
+
+const priceType = ref<OptionPrice[]>([])
+const limits = ref<ILimits>()
+
+const graphData = computed<ChartData<'line'>>(() => ({
+        datasets: datasets.value,
+        labels: labels.value
+}))
+
+const datasets = computed(() => {
+    const rawdatasets = OptionCalculator.graphDataForComparing(
+        props.models,
+        priceType.value,
+        end.value
+    )
+
+    const result = rawdatasets.map(dataset => ({
+        label: `${dataset.modelName} ${dataset.priceType}`,
+        data: dataset.data.splice(start.value, end.value)
+    }))
+
+    return result
+})
+
+const labels = computed<number[]>(() => {
+    const labels: number[] = []
+    for (let i = start.value; i < end.value; i++) {
+        labels.push(i)
+    }
+
+    return labels
+})
+
+const start = computed<number>(() => limits.value?.start || 0)
+const end = computed<number>(() => limits.value?.end || 20)
+
+const isNotEmptyPriceType = computed<boolean>(() => {
+    return priceType.value.length > 0
+})
 </script>
+<template>
+<div>
+    <FormKit type="price" v-model="priceType" />
+    <FormKit type="limits" v-model="limits" />
+    <Line 
+        v-if="isNotEmptyPriceType"
+        :chart-data="graphData"
+    />
+</div>
+</template>
