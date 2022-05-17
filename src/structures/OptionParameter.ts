d3f8f2f6c6fc1e935d75ba8e707da723e8b29e65
toombez <timurtokaev.toombez@gmail.com>
2022-05-18 01:16:12 +0300

    Add OptionParameter class
    Class used for registration new option parameters classes.
    Every inherited class must implement fromRaw method.
    Inherited class can represent any option parameter type
    because of "T" generic.

---
+++ src/structures/OptionParameter.ts
@@ -0,0 +1,35 @@
+interface IOptionParameter<T> {
+    name: string;
+    label: string;
+    description: string;
+
+    defaultValue: T;
+    min?: T;
+    max?: T;
+    step?: number;
+}
+
+export default abstract class OptionParameter<T> implements IOptionParameter<T> {
+    static registred = new Map<string, OptionParameter<unknown>>();
+    
+    public readonly label!: string;
+    public readonly name!: string;
+    public readonly description!: string;
+
+    public readonly defaultValue!: T;
+    public readonly min?: T | undefined;
+    public readonly max?: T | undefined;
+    public readonly step?: number;
+
+    constructor(parameter: IOptionParameter<T>) {
+        const registred = OptionParameter.registred.get(parameter.name)
+        if (registred) {
+            throw new Error(`"${parameter.name}" option parameter already registred.`)
+        }
+
+        Object.assign(this, parameter)
+        OptionParameter.registred.set(parameter.name, this)
+    }
+
+    abstract fromRaw(rawValue: string): T;
+}
