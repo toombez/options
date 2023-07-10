2fad70e25d2a25313fbcb8a831004ba9dbcd7816
Timur <timurtokaev.toombez@gmail.com>
2023-07-11 00:36:12 +0300

    Add example option for BS

--- apps/web/app.vue
+++ apps/web/app.vue
@@ -1,5 +1,29 @@
+<script setup lang="ts">
+import { UnderlyingAssetBuilder, OptionBuilder, BSM } from '@options/lib'
+
+const option = new OptionBuilder()
+    .setUnderlyingAsset(new UnderlyingAssetBuilder()
+        .setName('lorem 1')
+        .setPrice(300)
+        .setVolatility(0.15)
+        .setDividendYield(0.03)
+        .build()
+    )
+    .setPremium(22)
+    .setExpirationDate(new Date('10.07.2024'))
+    .setDate(new Date('10.07.2023'))
+    .setRiskFreeRate(0.03)
+    .setType('PUT')
+    .setStrikePrice(250)
+    .build()
+
+const bsm = new BSM(option, 365)
+</script>
+
 <template>
     <div>
-        <NuxtWelcome />
+        BS model call: {{ bsm.callPrice }}
+        <br>
+        BS model put: {{ bsm.putPrice }}
     </div>
 </template>
