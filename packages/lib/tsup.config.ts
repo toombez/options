4dac391d0253f46dc7e110729298f485e93e2123
Timur <timurtokaev.toombez@gmail.com>
2023-07-10 22:22:25 +0300

    Types file on build

--- packages/lib/tsup.config.ts
+++ packages/lib/tsup.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'tsup'
 
 defineConfig((options) => ({
+    dts: true,
+    bundle: true,
+    format: ['esm', 'cjs'],
     minify: !options.watch,
     clean: !options.watch,
-    dts: true,
+    treeshake: !options.watch,
 }))
 
