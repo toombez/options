fec3a4e678cb617240e363e3eee9e97f1ef5d2c5
Timur <toombez.timurtokaev@gmail.com>
2024-06-01 02:24:04 +0300

    Add types with refinement and chrono for options

--- packages/lib/src/finance_option.rs
+++ packages/lib/src/finance_option.rs
@@ -1,3 +1,41 @@
+use chrono::{DateTime, Utc};
+use refinement::{Predicate, Refinement};
+use crate::asset::UnderlyingAsset;
+
+pub struct FloatInRangeFromZeroToOneHundred;
+
+impl Predicate<f32> for FloatInRangeFromZeroToOneHundred {
+    fn test(x: &f32) -> bool {
+        (0.0..100.0).contains(x)
+    }
+}
+
+pub struct FloatMoreThanZero;
+
+impl Predicate<f32> for FloatMoreThanZero {
+    fn test(x: &f32) -> bool {
+        *x > 0.0
+    }
+}
+
+pub type RiskFreeInterestRate = Refinement<
+    f32,
+    FloatInRangeFromZeroToOneHundred,
+>;
+
+pub type Volatility = Refinement<
+    f32,
+    FloatInRangeFromZeroToOneHundred,
+>;
+
+pub type StrikePrice = Refinement<
+    f32,
+    FloatMoreThanZero,
+>;
+
+pub type ExpirationDate = DateTime<Utc>;
+
+#[derive(Debug, Clone, Copy)]
 pub enum FinanceOptionType {
     Call,
     Put,
