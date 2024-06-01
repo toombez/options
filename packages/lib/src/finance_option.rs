04209e345ef732247a7982bad342fbf05ed66cd3
Timur <toombez.timurtokaev@gmail.com>
2024-06-01 19:13:44 +0300

    Use new structs for FinanceOption

--- packages/lib/src/finance_option.rs
+++ packages/lib/src/finance_option.rs
@@ -1,24 +1,13 @@
-use chrono::{DateTime, Utc};
-use refinement::Refinement;
+use chrono::{DateTime, TimeZone, Utc};
+use derive_builder::Builder;
+
 use crate::asset::UnderlyingAsset;
-use crate::utils::{FloatInRangeFromZeroToOneHundred, FloatMoreThanZero};
+use crate::utils::{FloatFromZero, FloatFromZeroToOneHundred};
 
-pub type RiskFreeInterestRate = Refinement<
-    f32,
-    FloatInRangeFromZeroToOneHundred,
->;
-
-pub type Volatility = Refinement<
-    f32,
-    FloatInRangeFromZeroToOneHundred,
->;
-
-pub type StrikePrice = Refinement<
-    f32,
-    FloatMoreThanZero,
->;
-
-pub type ExpirationDate = DateTime<Utc>;
+pub type Volatility = FloatFromZeroToOneHundred;
+pub type StrikePrice = FloatFromZero;
+pub type RiskFreeInterestRate = FloatFromZeroToOneHundred;
+pub type ExpirationDate<T> = DateTime<T>;
 
 #[derive(Debug, Clone, Copy)]
 pub enum FinanceOptionType {
@@ -26,12 +15,13 @@ pub enum FinanceOptionType {
     Put,
 }
 
-#[derive(Debug, Clone)]
-pub struct FinanceOption {
+#[derive(Debug, Clone, Builder)]
+pub struct FinanceOption<T = Utc>
+where T: TimeZone {
     pub option_type: FinanceOptionType,
     pub underlying_asset: UnderlyingAsset,
-    pub strike_price: f32,
-    pub risk_free_interest_rate: f32,
-    pub volatility: f32,
-    pub expiration_date: ExpirationDate,
+    pub strike_price: StrikePrice,
+    pub risk_free_interest_rate: RiskFreeInterestRate,
+    pub volatility: Volatility,
+    pub expiration_date: ExpirationDate<T>,
 }
