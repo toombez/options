88227932a1ca4b88a0ef322e280836b29de94530
Timur <toombez.timurtokaev@gmail.com>
2024-06-01 18:55:05 +0300

    Add utility structs for subset of float numbers

--- packages/lib/src/utils.rs
+++ packages/lib/src/utils.rs
@@ -1,16 +1,71 @@
-pub struct FloatInRangeFromZeroToOneHundred;
-use refinement::Predicate;
+use std::fmt::Error;
+use num::traits::bounds::{LowerBounded, UpperBounded};
 
-impl Predicate<f32> for FloatInRangeFromZeroToOneHundred {
-    fn test(x: &f32) -> bool {
-        (0.0..100.0).contains(x)
+pub trait HiddenValue<T>: Sized {
+    fn value(&self) -> T;
+    fn is_valid(value: T) -> bool;
+    fn parse(value: T) -> Result<Self, Error>;
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct FloatInRangeFromZeroToOneHundred(f64);
+
+impl LowerBounded for FloatInRangeFromZeroToOneHundred {
+    fn min_value() -> Self {
+        Self(0.0)
     }
 }
 
-pub struct FloatMoreThanZero;
+impl UpperBounded for FloatInRangeFromZeroToOneHundred {
+    fn max_value() -> Self {
+        Self(100.0)
+    }
+}
 
-impl Predicate<f32> for FloatMoreThanZero {
-    fn test(x: &f32) -> bool {
-        *x > 0.0
+impl HiddenValue<f64> for FloatInRangeFromZeroToOneHundred {
+    fn value(&self) -> f64 {
+        self.0
+    }
+
+    fn is_valid(value: f64) -> bool {
+        let min_value = Self::min_value().value();
+        let max_value = Self::max_value().value();
+
+        value >= min_value && value <= max_value
+    }
+
+    fn parse(value: f64) -> Result<FloatInRangeFromZeroToOneHundred, Error> {
+        if Self::is_valid(value) {
+            Ok(Self(value))
+        } else {
+            Err(Error)
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct FloatFromZero(f64);
+
+impl LowerBounded for FloatFromZero {
+    fn min_value() -> Self {
+        Self(0.0)
+    }
+}
+
+impl HiddenValue<f64> for FloatFromZero {
+    fn value(&self) -> f64 {
+        self.0
+    }
+
+    fn is_valid(value: f64) -> bool {
+        value >= 0.0
+    }
+
+    fn parse(value: f64) -> Result<Self, Error> where Self: Sized {
+        if Self::is_valid(value) {
+            Ok(Self(value))
+        } else {
+            Err(Error)
+        }
     }
 }
